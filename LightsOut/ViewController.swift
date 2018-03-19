b4c966d10b4a0014ec9dce4d4455734c0aa47510
wlucas7474 <weierlj@rose-hulman.edu>
2018-03-19 17:54:36 -0400

    Changes were made

--- LightsOut/ViewController.swift
+++ LightsOut/ViewController.swift
@@ -10,14 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gameStateLabel: UILabel!
+    @IBOutlet var gameButtons: [UIButton]!
+    @IBOutlet weak var gameStateNavBar: UINavigationBar!
+    
+    var game = LinearLightsOutGame(numLights: 13)
+    
+    @IBAction func pressedNewGame(_ sender: Any) {
+        print("new game")
+        game = LinearLightsOutGame(numLights: 13)
+        updateView(game.checkForGameOver())
+    }
+    
+    @IBAction func pressedGameButton(_ sender: Any) {
+        let gameBoardButton = sender as! UIButton
+        print(gameBoardButton.tag);
+        updateView(game.pressedLightAtIndex(gameBoardButton.tag))
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateView(game.checkForGameOver())
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateView(_ gameState: Bool) {
+        let onImage = #imageLiteral(resourceName: "light_on")
+        let offImage = #imageLiteral(resourceName: "light_off")
+        
+        for i in 0..<game.lightStates.count {
+            var button: UIButton
+            if (traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact) {
+                button = gameButtons[i]
+            } else {
+                button = gameButtons[i + 13]
+            }
+            switch(game.lightStates[i]) {
+            case true:
+                button.setImage(onImage, for: UIControlState.normal)
+            case false:
+                button.setImage(offImage, for: UIControlState.normal)
+            }
+        }
     }
 
 
