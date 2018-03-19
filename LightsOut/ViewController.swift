1c62a9b448faad3b8dbfd4ceeb08ecc0a16889ec
wlucas7474 <weierlj@rose-hulman.edu>
2018-03-19 13:56:34 -0400

    Initial Commit

---
+++ LightsOut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightsOut
+//
+//  Created by CSSE Department on 3/19/18.
+//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
